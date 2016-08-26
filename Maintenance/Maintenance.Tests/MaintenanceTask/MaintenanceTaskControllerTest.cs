2b9c36444c66f2af15a14ed19a41b6a90b66898f
IEUser <IEUser>
2016-08-26 16:37:57 -0600

    Initial Commit

---
+++ Maintenance/Maintenance.Tests/MaintenanceTask/MaintenanceTaskControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Maintenance.Tests.MaintenanceTask
+{
+    [TestClass]
+    public class MaintenanceTaskControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Maintenance/Maintenance/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Maintenance.WebApiApplication" Language="C#" %>
